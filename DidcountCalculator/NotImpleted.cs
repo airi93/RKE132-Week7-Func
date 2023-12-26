2d294680079a21d57c43e9ebd1d074c10068709e
Irina Belik. <ibelik@tktk.ee>
2023-12-26 15:19:04 +0200

    add 7week code

---
+++ DidcountCalculator/NotImpleted.cs
@@ -0,0 +1,27 @@
+﻿//total < 10 - 1%
+//total >= 10 && total < 20 - 5%
+//total >= 20 - 10%
+
+
+
+using System.Runtime.Serialization;
+
+[Serializable]
+internal class NotImpleted : Exception
+{
+    public NotImpleted()
+    {
+    }
+
+    public NotImpleted(string? message) : base(message)
+    {
+    }
+
+    public NotImpleted(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected NotImpleted(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
